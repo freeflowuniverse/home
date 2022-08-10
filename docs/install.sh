95f43db2f27dfeaedc697fcda660da2df1892536
kristof de spiegeleer <kristof@incubaid.com>
2022-08-09 23:37:33 -0700

    Update install.sh

--- docs/install.sh
+++ docs/install.sh
@@ -9,6 +9,6 @@ fi
 if ! [ -x "$(command -v mdbook)" ]; then
     cargo install mdbook
     cargo install mdbook-mermaid
-    cd.grants/twin_backend
+    cd grants/twin_backend
     mdbook-mermaid install .
 fi
